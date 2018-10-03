5faef38d0ae1fb12ac3c44111d52282b7002dd97
Nate Miller <natekmiller96@gmail.com>
2018-10-03 21:44:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue, arg);
 }
 
-function setThisWithCall(fn, thisValue, args){
+function setThisWithApply(fn, thisValue, args){
   return fn.apply(thisValue, [args]);
 }
